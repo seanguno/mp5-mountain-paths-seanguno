381cb154d12c5f1eef2b7e24c2a58fa45f652ad1
Sean Guno <91289730+seanguno@users.noreply.github.com>
2023-08-19 22:56:07 -0500

    Update color.cc

--- src/color.cc
+++ src/color.cc
@@ -1,11 +1,24 @@
 #include "color.hpp"
+#include <stdexcept>
 
 Color::Color(int r, int g, int b): red_(r), green_(g), blue_(b) {
-  // need to ensure valid color has been constructed...
+  if (!IsValid(r)) {
+    throw std::runtime_error("invalid color value");
+  }
+  if (!IsValid(g)) {
+    throw std::runtime_error("invalid color value");
+  }
+  if (!IsValid(b)) {
+    throw std::runtime_error("invalid color value");
+  }
 }
 
 // do not modify
 bool operator==(const Color& rhs, const Color& lhs) {
   return (rhs.Red() == lhs.Red() && rhs.Green() == lhs.Green() &&
           rhs.Blue() == lhs.Blue());
+}
+
+bool Color::IsValid(int value) {
+  return value >= kColorValueMin && value <= kColorValueMax;
 }
