2312dc9d1755c8243dd216db06b0443fcad9baa1
Sean Guno <91289730+seanguno@users.noreply.github.com>
2023-08-19 22:54:14 -0500

    Update elevation_dataset.hpp

--- includes/elevation_dataset.hpp
+++ includes/elevation_dataset.hpp
@@ -3,16 +3,26 @@
 
 #include <cstdlib>  // for size_t
 #include <vector>
-
+#include <string>
+#include <fstream>
+#include <limits>
 class ElevationDataset {
 public:
-  // write behavior declarations here; define in elevation_dataset.cc.
+  ElevationDataset(const std::string& filename, size_t width, size_t height);
+  size_t Width() const;
+  size_t Height() const;
+  int MaxEle() const;
+  int MinEle() const;
+  int DatumAt(size_t row, size_t col) const;
+  const std::vector<std::vector<int>>& GetData() const;
 private:
-  // size_t width_;
-  // size_t height_;
-  // std::vector<std::vector<int>> data_;
-  // int max_ele_;
-  // int min_ele_;
+  size_t width_;
+  size_t height_;
+  std::vector<std::vector<int>> data_;
+  int max_ele_;
+  int min_ele_;
+  int IsMax(int test, int max);
+  int IsMin(int test, int min);
 };
 
 #endif
